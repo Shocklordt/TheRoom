0417d49f860de0a30c2a37b510912f060d53b533
VirtuesL <caelanthestone@gmail.com>
2018-04-02 20:30:08 +0300

    testing again

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -26,7 +26,7 @@ const GetData = async () => {
 
 class Game extends React.Component {
   componentWillMount() {
-    this.state = {open: GetData()}
+    this.setState({open: GetData()})
     console.log(this.state.open)
   }
   render(){
