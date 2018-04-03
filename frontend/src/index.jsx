6fc6d1446016b713f633156984c1aae8a6bd732f
VirtuesL <caelanthestone@gmail.com>
2018-04-03 23:54:26 +0300

    final fix of sringify

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -54,7 +54,7 @@ class Game extends React.Component{
     for (var i = 0; i< arraylist.length; i++){
       tempdat.push(arraylist[i].temperature)
     }
-    this.setState({temperature: await tempdat.json()})
+    this.setState({temperature: await JSON.stringify(tempdat)})
   }
   render(){
       return(
